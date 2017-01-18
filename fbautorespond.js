c8adeec5d846a20f1a2eca4dd1e895b76a59b5f2
Jamie Kirkpatrick <jkp@spotify.com>
2017-01-18 15:03:42 +0100

    Add workaround to restart the app at an interval.
    This is needed for long-running instances due to a bug in the
    `facebook-chat-api` library.
    
    See https://github.com/Schmavery/facebook-chat-api/issues/202

--- fbautorespond.js
+++ fbautorespond.js
@@ -6,6 +6,7 @@ var argv = require('yargs')
     .describe('response', 'The response to send on new messages')
     .describe('email', 'The email address to login with')
     .describe('password', 'The password to authenticate with')
+    .describe('restart', 'An optional number of minutes to restart after')
     .demand('email')
     .demand('response')
     .argv;
@@ -15,30 +16,69 @@ if (!argv.password) {
   });
 }
 
-login(argv, function callback (err, api) {
-  if(err) {
-    return console.error(err);
+function autorespond() {
+  var stateFile = 'appstate.json';
+  if (fs.existsSync(stateFile)) {
+    delete argv.username, argv.password;
+    argv.appState = JSON.parse(fs.readFileSync(stateFile, 'utf8'));
   }
 
-  api.setOptions({
+  login(argv, function callback (err, api) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    fs.writeFileSync(stateFile, JSON.stringify(api.getAppState()));
+
+    api.setOptions({
       forceLogin: true,
       logLevel: "warn"
-  });
+    });
 
-  var threads = {};
-  api.listen(function callback(err, message) {
-    if (err) {
-      console.log(err);
-    } else if (message.type == 'message') {
-      var thread = threads[message.threadID] || {};
-      if (!(message.senderID in thread)) {
-      	console.log('Received message from new sender: ', message);
-	api.sendMessage(argv.response, message.threadID);
-	thread[message.senderID] = true;
-	threads[message.threadID] = thread;
-      } else {
-      	console.log('Ignoring message from known sender: ', message);
+    var threads = {};
+    api.listen(function callback(err, message) {
+      if (err) {
+        console.log(err);
+      } else if (message.type == 'message') {
+        var thread = threads[message.threadID] || {};
+        if (!(message.senderID in thread)) {
+          console.log('Received message from new sender: ', message);
+          api.sendMessage(argv.response, message.threadID);
+          thread[message.senderID] = true;
+          threads[message.threadID] = thread;
+        } else {
+          console.log('Ignoring message from known sender: ', message);
+        }
       }
+    });
+  });
+}
+
+// The keep-alive parameter forces the script to restart on at a given
+// interval: this is currently needed for long-running instances because
+// of a bug in the underlying library we are using.
+// https://github.com/Schmavery/facebook-chat-api/issues/202
+if (argv['restart']) {
+  var fork = require('child_process').fork;
+  var timeout = argv['restart'] * 60 * 1000;  // timeout in ms
+
+  process.env['FB_AUTORESPOND_EMAIL'] = argv.email;
+  process.env['FB_AUTORESPOND_PASSWORD'] = argv.password;
+  process.env['FB_AUTORESPOND_RESPONSE'] = argv.response;
+
+  var child = fork(argv['$0'], []);
+  var i = 0;
+  setInterval(function() {
+    console.log('Restarting after ' + ++i * argv['restart'] + ' minutes');
+    child.kill();
+    child = fork(argv['$0'], []);
+  }, timeout);
+
+  child.on('close', (code, signal) => {
+    if (code && !signal) {
+      process.exit(code);
     }
   });
-});
+} else {
+  autorespond();
+}
